ee99bf4a896386969af0c5be0018d15790be2710
Kelvin Korir <kibetkelvin222@gmail.com>
2026-05-18 11:57:30 +0300

    added a PATCH to update product

--- juicy-app/src/services/api.js
+++ juicy-app/src/services/api.js
@@ -54,3 +54,24 @@ export const addProduct = async (product) => {
         console.error("Error adding product", error);
     }
 };
+
+//PATCH-update part of a product
+export const updateProduct = async (IdleDeadline, updateData) => {
+    try {
+        const res = await fetch(`${BASE_URL}/juice/${id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(updateData)
+        });
+
+        if (!res.ok) {
+            throw new Error("Failed to update product");
+        }
+
+        return await res.json();
+    } catch (error) {
+        console.error("Error updating product");
+    }
+};
