b498c0c51b795acb32bbee52cb8563088d60f0d5
Kelvin Korir <kibetkelvin222@gmail.com>
2026-05-18 12:47:24 +0300

    dynamic filtering and context API

--- juicy-app/src/pages/ShopPage.jsx
+++ juicy-app/src/pages/ShopPage.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { useContext, useState } from "react";
+import { ProductContext } from "../context/ProductContext";
+import SideBar from "../components/ProductGrid";
+import ProductGrid from "../components/ProductGrid";
 
 function ShopPage() {
+    //get global products from context
+    const { products } = useContext(ProductContext);
+    //local state for search input
+    const [search, setSearch] = useState("");
+
+    //filter logic
+    const filteredProducts = products.filter(product => product.name.toLowerCase().includes(search.toLowerCase()));
   return (
-    <h1>Shop</h1>
-  )
+    <div>
+      <h1>Shop</h1>
+      <SideBar search={search} setSearch={setSearch} /> 
+      <ProductGrid products={filteredProducts} />   
+    </div>
+    
+  );
 }
 
 export default ShopPage
