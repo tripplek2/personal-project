b3c8eb2699abcc1151741e2279c49d6cb9cd1c7b
Kelvin Korir <kibetkelvin222@gmail.com>
2026-05-18 10:30:24 +0300

    rendered navbar in app.jsx and added paragraph in home

--- juicy-app/src/App.jsx
+++ juicy-app/src/App.jsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import HomePage from "./pages/HomePage"
 import ShopPage from "./pages/ShopPage"
 import AdminPage from "./pages/AdminPage"
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <BrowserRouter>
+       <NavBar />
        <Routes>
          <Route path="/" element={<HomePage />} />
          <Route path="/shop" element={<ShopPage />} />
--- juicy-app/src/pages/HomePage.jsx
+++ juicy-app/src/pages/HomePage.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function HomePage() {
   return (
-    <h1>Home</h1>
+    <div>
+      <h1>Juice it!</h1>
+      <p>Welcome to pure,cold-pressed nutrition made simple.We blend 100% organic fruits into delicious juices that boost your energy and support your health.No added sugar.No artificial ingredients just raw and refreshing.</p>
+
+    </div>
+   
   )
 }
 
