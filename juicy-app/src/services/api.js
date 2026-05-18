d7704597aca60b5eae53bc5d7dafb88536fd2aed
Kelvin Korir <kibetkelvin222@gmail.com>
2026-05-18 11:42:10 +0300

    added a GET fetch for store info and products

---
+++ juicy-app/src/services/api.js
@@ -0,0 +1,36 @@
+const BASE_URL = "http://localhost:3001/";
+
+// store info
+
+// GET-fetch store details
+export const getStoreInfo = async () => {
+    try {
+        const res = await fetch(`${BASE_URL}/store-info`);
+
+        if (!res.ok) {
+            throw new Error("Failed to fetch");
+        }
+        return await res.json();
+    } catch (error) {
+        console.error("Error fetching store info");
+        return [];
+    }
+};
+
+// Product info
+
+// GET-fetch all products
+export const getProducts = async () => {
+    try {
+        const res = await fetch(`${BASE_URL}/juice`);
+
+        if(!res.ok) {
+            throw new Error("Failed to fetch products");
+        }
+
+        return await res.json();
+    } catch (error) {
+        console.error("Error fetching products:", error);
+        return [];
+    }
+};
