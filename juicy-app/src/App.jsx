68b584bfdb4c64c6ebe41c03f4c69a6631cd6a05
Kelvin Korir <kibetkelvin222@gmail.com>
2026-05-18 09:38:02 +0300

    initial commit

---
+++ juicy-app/src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Shop from "./pages/Shop"
+import Admin from "./pages/Admin"
+
+function App() {
+  return (
+    <BrowserRouter>
+       <Routes>
+         <Route path="/" element={<Home />} />
+         <Route path="/shop" element={<Shop />} />
+         <Route path="/admin" element={<Admin />} />
+       </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
