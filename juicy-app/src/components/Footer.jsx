0726b4c2c86baa3d71df9d767ffdb19eb7f579b7
Kelvin Korir <kibetkelvin222@gmail.com>
2026-05-19 16:38:31 +0300

    add footer

---
+++ juicy-app/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <div>
+        <footer className="footer">
+            <p>© {new Date().getFullYear()} Juice It! All rights reserved.</p>
+            <p>Fresh juices. Healthy living.</p>
+        </footer>
+      
+    </div>
+  )
+}
+
+export default Footer;
