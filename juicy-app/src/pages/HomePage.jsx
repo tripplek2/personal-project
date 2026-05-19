d7dbb4b516dc235e4d56842d526c54befd8066af
Kelvin Korir <kibetkelvin222@gmail.com>
2026-05-19 20:04:45 +0300

    added some styles and a classname

--- juicy-app/src/pages/HomePage.jsx
+++ juicy-app/src/pages/HomePage.jsx
@@ -1,9 +1,9 @@
 
 function HomePage() {
   return (
-    <div className="home">
+    <div className="home-page">
       <h1>Juice it!</h1>
-      <p>Welcome to pure,cold-pressed nutrition made simple.We blend 100% organic fruits into delicious juices that boost your energy and support your health.No added sugar.No artificial ingredients just raw and refreshing.</p>
+      <p className="home-text">Welcome to pure,cold-pressed nutrition made simple. <br />We blend 100% organic fruits into delicious juices that boost your energy and support your health. <br />No added sugar.No artificial ingredients just raw and refreshing.</p>
 
     </div>
    
